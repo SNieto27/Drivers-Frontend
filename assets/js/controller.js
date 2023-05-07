d799fbca07c880f21cb29c3e5f79c1997d736198
Saúl Nieto <nietopavon1@gmail.com>
2023-05-07 10:14:05 -0600

    Con backend

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,3 +1,5 @@
+var motoristas = [];
+
 const mostrarSeccionLogIn = () => {
     document.getElementById('seccion-login').style.display = "block";
     document.getElementById('seccion-signin').style.display = "none";
@@ -12,4 +14,31 @@ const mostrarSeccionSignIn = () => {
 
 irPrincipal = () => {
     window.location.href = "principal.html"; 
+}
+
+const obtenerMotoristas = () => {
+    fetch('http://localhost:8000/motoristas', {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json",
+        }
+    })
+    .then((respuesta) => respuesta.json())
+    .then((respuestaMotoristas) => {
+        console.log(respuestaMotoristas);
+        motoristas = respuestaMotoristas;
+    })
+}
+
+obtenerMotoristas();
+
+const logIn = () => {
+    let correo = document.getElementById("correo-login").value;
+    let clave = document.getElementById("pass-login").value;
+    motoristas.forEach(motorista => {
+        if (correo == motorista.email && clave == motorista.password) {
+            localStorage.setItem("_id", motorista._id);
+            window.location.href = "principal.html"
+        } 
+    });
 }
